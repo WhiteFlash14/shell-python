d3c099956cc56a02c603434f4c5dec3882392924
nish <nishchal1405@gmail.com>
2025-08-27 17:21:37 +0530

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -3,28 +3,48 @@ import os
 
 
 def main():
-    # Uncomment this block to pass the first stage
-    
+   builtin_commands=["echo","exit","type"]
 
-    while True:
+   while True:
      sys.stdout.write("$ ")
 
     # Wait for user input
     
      command = input()
-     builtin_commands=["echo","exit","type"]
+     paths = os.environ["PATH"].split(":")
+     
+     
+     
+
+     
      
      if command == "exit 0":
         sys.exit(0)
      elif command.startswith("echo"):
         print(command[5:])
      elif command.startswith("type"):
-        if command[5:] in builtin_commands:
-           print(f"{command[5:]} is a shell builtin")
-        else:
-           print(f"{command[5:]}: not found")
+        cmd = command[5:].strip()
+        if cmd in builtin_commands:
+            print(f"{cmd} is a shell builtin")
+            continue
+        found = False
+        for directory in paths:
+           # abs_path = os.path.abspath(path)
+           if not os.path.isdir(directory):
+              continue
+           full_path = os.path.join(directory, cmd)
+           if os.path.isfile(full_path) and os.access(full_path, os.X_OK):
+                print(f"{cmd} is {full_path}")
+                found = True
+                break
+        
+        if not found:
+           print(f"{cmd}: not found")
      else:
       print(f"{command}: command not found")
+    
+
+    
     
 
 if __name__ == "__main__":
