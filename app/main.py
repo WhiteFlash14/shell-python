d552120b3942b59d63d7be77b5c4d7842c65185a
nish <nishchal1405@gmail.com>
2025-09-05 01:34:03 +0530

    refactored code and added pwd functionality

--- app/main.py
+++ app/main.py
@@ -2,79 +2,88 @@ import sys
 import os
 import subprocess
 
-
-def main():
-   builtin_commands=["echo","exit","type"]
-
-   while True:
-    sys.stdout.write("$ ")
-   #  sys.stdout.flush()
-
-    command = input()
-    parts = command.split()
-    cm = parts[0]
-    args = parts[1:]
-    paths = os.environ["PATH"].split(":")
-    
-    executables=[]
-    for directory in paths:
+def find_executable(cmd):
+   paths = os.environ["PATH"].split(":")
+   for directory in paths:
         # abs_path = os.path.abspath(path)
         if not os.path.isdir(directory):
            continue
-        for f in os.listdir(directory):
-            full_path = os.path.join(directory, f)
-            if os.path.isfile(full_path) and os.access(full_path, os.X_OK):
-                executables.append((f, full_path))
+        
+        full_path = os.path.join(directory, cmd)
+        if os.path.isfile(full_path) and os.access(full_path, os.X_OK):
+             return full_path
+   return None
         # full_path = os.path.join(directory, cmd)
         # if os.path.isfile(full_path) and os.access(full_path, os.X_OK):
              
         #      found = True
         #      break
     #print(executables)
-     
+def main():
+   builtin_commands=["echo","exit","type","pwd"]
+  
 
-     
-    exec_command = False 
-    type_executed = None
-    echo_executed=None
-    if command == "exit 0":
-       sys.exit(0)
+   while True:
+    sys.stdout.write("$ ")
+    sys.stdout.flush()
+
+    command = input()
+    parts = command.split()
+    cm = parts[0]
+    args = parts[1:] 
+
+    if cm == "exit":
+       if len(cm)>1 and args[0].isdigit():
+          sys.exit(int(args[0]))
+       else:
+          sys.exit(0)
     elif command.startswith("echo"):
-       print(command[5:])
-       echo_executed=True
+      #  print(command[5:])
+      sys.stdout.write(' '.join(args)+"\n")
+      echo_executed=True
+      #  continue
     elif command.startswith("type"):
-       cmd = command[5:].strip()
-       full_path = os.path.join(directory, cmd)
+       cmd = args[0]
        if cmd in builtin_commands:
            print(f"{cmd} is a shell builtin")
            continue
        
-       for name,path in executables:
-          if name == cmd:
-             print(f"{cmd} is {path}")
-             exec_command=True
-             break
-
-       if not exec_command :
-         print(f"{cmd}: not found")
-         type_executed = True
+       
+       path = find_executable(cmd)
+       if path:
+          print(f"{cmd} is {path}")
+          
+       else:
+            print(f"{cmd}: not found")
+         # type_executed = True
     
-    found = None
+    elif cm =="pwd":
+       print(os.getcwd())
+      #  continue
     
-    for name,path in executables:
-       if name ==cm:
-          found = path
-          break
-    if found and not echo_executed:
-       try:
-          result = subprocess.run(parts,capture_output=True,text=True) 
-          print(result.stdout.rstrip())  
-       except Exception as e:
-          print(f"Error running {cm}: {e}")
+    #  for name,path in executables:
+    #     if name ==cm:
+    #        found = path
+    #        break
+    #  if found and not echo_executed:
+    #     try:
+   #        result = subprocess.run(parts,capture_output=True,text=True) 
+   #        print(result.stdout.rstrip())  
+   #     except Exception as e:
+   #        print(f"Error running {cm}: {e}")
     
-    elif not exec_command and type_executed == None and not echo_executed:
-     print(f"{command}: command not found")
-    
+   #  elif not exec_command and type_executed == None and not echo_executed:
+   #   print(f"{command}: command not found")
+    else:
+       executable_path = find_executable(cm)
+       if executable_path:
+          try:
+             result = subprocess.run(parts,capture_output=True,text=True) 
+             print(result.stdout.rstrip())  
+          except Exception as e:
+             print(f"Error running {cm}: {e}")
+       else:
+          print(f"{command}: command not found")
     
     
     
